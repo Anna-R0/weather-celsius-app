fe874a227e95a4d8d9388e49c2d0a5e88f3bc6f0
Anna <eviltwin007@yandex.ru>
2022-10-20 19:20:31 -0700

    Added github URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { waitForElementToBeRemoved } from "@testing-library/react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Anna Ro and is
+        <a
+          href="https://github.com/Anna-R0/weather-celsius-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced at github
+        </a>
+      </footer>
+    </div>
+  );
 }
